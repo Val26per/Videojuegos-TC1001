6ee095b5cd8cf1da8c1c2d29761fe896e017aaa6
samagrm <105225625+samagrm@users.noreply.github.com>
2022-09-16 13:30:32 -0500

    a

--- act2.py
+++ act2.py
@@ -1,11 +1,17 @@
 from turtle import *
+import random
 from random import randrange
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
-
+colors = ["yellow","pink","purple","light blue","green"]
+cb = random.choice(colors)
+cf = random.choice(colors)
+places = ['left', 'right', 'up', 'down']
+while cb == cf:
+    cf = random.choice(colors)
 def change(x, y):
     "Change snake direction."
     aim.x = x
@@ -15,13 +21,17 @@ def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+def dentro(food):
+    return -200 < food.x < 190 and -200 < food.y < 190
+
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+        for i in range (1):
+            square(head.x, head.y, 9,'red')
         update()
         return
 
@@ -31,15 +41,32 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+        ##food.goto(food.x, food.y)
     else:
         snake.pop(0)
-
+        
+    pos = random.choice(places)
+    if not dentro(food):
+        food.x = randrange(-15, 15) * 10 
+        food.y = randrange(-15, 15) * 10
+    elif pos == 'left':
+        food.x -=10
+    elif pos == 'right':
+        food.x +=10
+    elif pos == 'up':
+        food.y += 10
+    else:
+        food.y -= 10
+    
     clear()
-
+            
+             
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9,cb)
 
-    square(food.x, food.y, 9, 'green')
+    ##square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9,cf)
+    
     update()
     ontimer(move, 100)
 
